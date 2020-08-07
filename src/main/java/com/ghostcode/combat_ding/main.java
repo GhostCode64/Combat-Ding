0e8c2f004efd047d0c21aaef28901ea63634ea81
GhostCode <ghostcodedev@gmail.com>
2020-08-08 00:56:03 +0100

    started work on playing sound

--- src/main/java/com/ghostcode/combat_ding/main.java
+++ src/main/java/com/ghostcode/combat_ding/main.java
@@ -1,21 +1,34 @@
 package com.ghostcode.combat_ding;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.sound.Sound;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.registry.Registry;
 
 public class main implements ModInitializer {
-	// an instance of our new item
-	public static final Item FABRIC_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+	SoundInstance ding = new SoundInstance() {
+	}
+
+			//new SoundIns("entity.experience_orb.pickup", 100.0f, 50.0f, 1, Sound.RegistrationType.SOUND_EVENT, false, true, 100);
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		Registry.register(Registry.ITEM, new Identifier("tutorial", "fabric_item"), FABRIC_ITEM);
+
+		System.out.println("Ding volume level: " + ding.getVolume());
+
+		PositionedSoundInstance sound = PositionedSoundInstance.master(soundEvent, Pling.getConfig().pitch);
+
+		MinecraftClient.getInstance().getSoundManager().play(ding);
 
 		System.out.println("Hello Fabric world!");
 	}

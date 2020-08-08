10f11692b4854278a26b9771c19e63411c1c3474
GhostCode <ghostcodedev@gmail.com>
2020-08-08 12:26:50 +0100

    removed some experiementation

--- src/main/java/com/ghostcode/combat_ding/main.java
+++ src/main/java/com/ghostcode/combat_ding/main.java
@@ -13,23 +13,14 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.registry.Registry;
 
 public class main implements ModInitializer {
-	SoundInstance ding = new SoundInstance() {
-	}
-
-			//new SoundIns("entity.experience_orb.pickup", 100.0f, 50.0f, 1, Sound.RegistrationType.SOUND_EVENT, false, true, 100);
+	Sound ding = new Sound("entity.experience_orb.pickup", 100.0f, 50.0f, 1, Sound.RegistrationType.SOUND_EVENT, false, true, 100);
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		System.out.println("Ding volume level: " + ding.getVolume());
-
-		PositionedSoundInstance sound = PositionedSoundInstance.master(soundEvent, Pling.getConfig().pitch);
-
-		MinecraftClient.getInstance().getSoundManager().play(ding);
-
+		
 		System.out.println("Hello Fabric world!");
 	}
 }

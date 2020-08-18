32188fe84023aa43c4d47b633920af2cb1c5d9c5
GhostCode <ghostcodedev@gmail.com>
2020-08-18 20:55:29 +0100

    new icon

--- src/main/java/com/ghostcode/combat_ding/main.java
+++ src/main/java/com/ghostcode/combat_ding/main.java
@@ -1,6 +1,8 @@
 package com.ghostcode.combat_ding;
 
+import com.ghostcode.combat_ding.mixin.click;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.sound.Sound;
@@ -8,9 +10,17 @@ import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class main implements ModInitializer {
 	Sound ding = new Sound("entity.experience_orb.pickup", 100.0f, 50.0f, 1, Sound.RegistrationType.SOUND_EVENT, false, true, 100);
